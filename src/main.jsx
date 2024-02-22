ea8000d487a22e733ff40267fffd9573b5787af0
Kate-Lesnyak <catharinalesnyak@gmail.com>
2024-02-23 01:46:03 +0200

    adds favicon, update env.local, delets comments

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,10 @@ import ReactDOM from "react-dom/client";
 
 import App from "./App.jsx";
 
-// import "./index.css";
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Suspense fallback="...loading">
-    {/* <React.StrictMode> */}
-    <App />
-    {/* </React.StrictMode> */}
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
   </Suspense>
 );
--- src/shared/services/api.js
+++ src/shared/services/api.js
@@ -1,34 +1,10 @@
 import axios from "axios";
 
-// const BASE_URL = 'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/';
-const API_KEY = '5EBVZ2C4UUVJXYXRL646W5MYX';
-
-// async function getTrips(query) {
-// 	const { data } = await axios.get(`${BASE_URL}/${query}?unitGroup=metric&key=${API_KEY}&contentType=json`);
-// 	return data;
-// }
-
-// export { getTrips };
+const API_KEY = import.meta.env.VITE_API_KEY;
 
 const tripInstance = axios.create({
 	baseURL: "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline"
 })
-
-export const getTrip1 = async (query) => {
-	const { data } = await tripInstance.get(`/${query}?unitGroup=metric&key=${API_KEY}&contentType=json`);
-	return data;
-}
-
-export const getTrip2 = async (query) => {
-	const { data } = await tripInstance.get(`/${query}`, {
-		params: {
-			unitGroup: 'metric',
-			key: API_KEY,
-			contentType: 'json',
-		}
-	})
-	return data;
-}
 
 export const getTrip = async (city, startDate, endDate) => {
 	const { data } = await tripInstance.get(`/${city}/${startDate}/${endDate}`, {
@@ -52,10 +28,3 @@ export const getTripToday = async (city) => {
 	})
 	return data;
 }
-
-
-// API for getting forecast from - to for the city
-// https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/[city]/[date1]/[date2]?unitGroup=metric&include=days&key=YOUR_API_KEY&contentType=json
-
-// API for getting today’s weather for the city
-// https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/[city]/today?unitGroup=metric&include=days&key=YOUR_API_KEY&contentType=json
